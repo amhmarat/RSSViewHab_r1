a2f7700a0bb7f94dbd5d89a0d2e9798588f60d0a
Marat Aymaletdinov <amh_marat@mail.ru>
2018-02-05 18:00:39 +0300

    Удалена кнопка загрузки

--- app/src/main/java/ru/sberbank/ma/rssviewhab/MainActivity.java
+++ app/src/main/java/ru/sberbank/ma/rssviewhab/MainActivity.java
@@ -4,8 +4,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,20 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button hRss = (Button) findViewById(R.id.la_fetch_but);
         HabrPool.getInstance(this);
-
-        hRss.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                FragmentManager manager = getSupportFragmentManager();
-                Fragment recyclerFragment = manager.findFragmentById(R.id.fl_main_id);
-                if (recyclerFragment == null) {
-                    recyclerFragment = getFragment();
-                    manager.beginTransaction().add(R.id.fl_main_id, recyclerFragment, null).commit();
-                }
-            }
-        });
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment recyclerFragment = manager.findFragmentById(R.id.fl_main_id);
+        if (recyclerFragment == null) {
+            recyclerFragment = getFragment();
+            manager.beginTransaction().add(R.id.fl_main_id, recyclerFragment, null).commit();
+        }
     }
 
     protected Fragment getFragment() {
