acdb1d38f05f5bd23f14d172c17b2ef2bfcd8707
Marat Aymaletdinov <amh_marat@mail.ru>
2018-02-05 11:22:24 +0300

    Добавлена реализация класса массива

--- app/src/main/java/ru/sberbank/ma/rssviewhab/HabrPool.java
+++ app/src/main/java/ru/sberbank/ma/rssviewhab/HabrPool.java
@@ -4,6 +4,39 @@ package ru.sberbank.ma.rssviewhab;
  * Created by internet on 05.02.2018.
  */
 
-public class HabrPool {
+import android.content.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class HabrPool {
+    private static HabrPool sHabrPool;
+    private static List<Habr> mHabrList;
+
+    private HabrPool(Context context) {
+        mHabrList = new ArrayList<>();
+    }
+
+    public static HabrPool getInstance(Context context) {
+        if (mHabrList == null)
+            return sHabrPool = new HabrPool(context);
+        return sHabrPool;
+    }
+
+    public static List<Habr> getHabrList() {
+        return mHabrList;
+    }
+
+    public static Habr getHabr(UUID uuid) {
+        for (Habr habr : mHabrList)
+            if (habr.getUUID().equals(uuid))
+                return habr;
+        return null;
+    }
+
+    static void addHabr(Habr habr) {
+        mHabrList.add(habr);
+    }
 }
