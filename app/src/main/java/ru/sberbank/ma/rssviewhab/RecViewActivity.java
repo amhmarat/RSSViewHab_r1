38dc9ed7ccea4969a37e101690d341170e1df4fc
Marat Aymaletdinov <amh_marat@mail.ru>
2018-02-05 18:28:54 +0300

    Добавлена реализация класса RecViewActivity

--- app/src/main/java/ru/sberbank/ma/rssviewhab/RecViewActivity.java
+++ app/src/main/java/ru/sberbank/ma/rssviewhab/RecViewActivity.java
@@ -1,9 +1,26 @@
 package ru.sberbank.ma.rssviewhab;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.Fragment;
+
+import java.util.UUID;
+
 /**
  * Created by internet on 05.02.2018.
  */
 
 public class RecViewActivity extends MainActivity {
     protected static final String INTENT_ID = "INTENT_ID";
+
+    @Override
+    protected Fragment getFragment() {
+        return RecViewControl.getInstance();
+    }
+
+    public static Intent newIntent(Context context, UUID uuid) {
+        Intent intent = new Intent(context, RecViewActivity.class);
+        intent.putExtra(INTENT_ID, uuid);
+        return intent;
+    }
 }
