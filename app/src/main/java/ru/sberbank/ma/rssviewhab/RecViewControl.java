8abaf7ea32a6e63e3b83dd8ed8a4aa476b0437cb
Marat Aymaletdinov <amh_marat@mail.ru>
2018-02-05 18:12:15 +0300

    Добавлен класс RecViewControl

---
+++ app/src/main/java/ru/sberbank/ma/rssviewhab/RecViewControl.java
@@ -0,0 +1,11 @@
+package ru.sberbank.ma.rssviewhab;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by internet on 05.02.2018.
+ */
+
+public class RecViewControl extends Fragment {
+
+}
